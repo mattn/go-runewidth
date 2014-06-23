94dfa59e13c55e8970e07cd3237514d44ee85eb6
mattn <mattn.jp@gmail.com>
2014-06-23 10:12:26 +0900

    Add IsEastAsian()

---
+++ runewidth_posix.go
@@ -0,0 +1,67 @@
+// +build !windows
+
+package runewidth
+
+import (
+	"os"
+	"regexp"
+	"strings"
+)
+
+var reLoc = regexp.MustCompile(`^[a-z][a-z][a-z]?(?:_[A-Z][A-Z])?\.(.+)`)
+
+func IsEastAsian() bool {
+	locale := os.Getenv("LC_CTYPE")
+	if locale == "" {
+		locale = os.Getenv("LANG")
+	}
+
+	// ignore C locale
+	if locale == "POSIX" || locale == "C" {
+		return false
+	}
+	if len(locale) > 1 && locale[0] == 'C' && (locale[1] == '.' || locale[1] == '-') {
+		return false
+	}
+
+	charset := strings.ToLower(locale)
+	r := reLoc.FindStringSubmatch(locale)
+	if len(r) == 2 {
+		charset = r[1]
+	}
+
+	if strings.HasSuffix("@cjk_narrow") {
+		return false
+	}
+
+	pos := strings.IndexByte(charset, '@')
+	if pos >= 0 {
+		charset = charset[:pos]
+	}
+
+	mbc_max = 1
+	switch charset {
+	case "utf-8", "utf8":
+		mbc_max = 6
+	case "jis":
+		mbc_max = 8
+	case "eucjp":
+		mbc_max = 3
+	case "euckr", "euccn":
+		mbc_max = 2
+	case "sjis", "cp932", "cp51932", "cp936", "cp949", "cp950":
+		mbc_max = 2
+	case "big5":
+		mbc_max = 2
+	case "gbk", "gb2312":
+		mbc_max = 2
+	}
+
+	if mbc_max > 1 && (charset[0] != 'U'
+			|| strings.HasPrefix(locale, "ja")
+			|| strings.HasPrefix(locale, "ko")
+			|| strings.HasPrefix(locale, "zn"))) {
+		return true
+	}
+	return false
+}
